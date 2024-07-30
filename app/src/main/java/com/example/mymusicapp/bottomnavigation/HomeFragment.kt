641b39f888fd8e3e9e18e30502008f9936b5d961
duonghieu2907 <duonghieu2907@gmail.com>
2024-07-30 15:08:14 +0700

    Branch Commit: Initial Drawer Navigation

--- app/src/main/java/com/example/mymusicapp/bottomnavigation/HomeFragment.kt
+++ app/src/main/java/com/example/mymusicapp/bottomnavigation/HomeFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.mymusicapp.R
 
 class HomeFragment : Fragment() {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Reference to drawer layout
+        drawerLayout = requireActivity().findViewById(R.id.main)
+
+        // Avatar icon
+        val avatarIcon = view.findViewById<ImageView>(R.id.avatar_icon)
+        avatarIcon.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return view
     }
 }
