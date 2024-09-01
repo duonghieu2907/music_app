86f9368141b4d7890fa7cee5cc93b4960d6d0e07
Dương Ngọc Quang Khiêm <128200532+KhimDuong@users.noreply.github.com>
2024-09-01 16:24:37 +0700

    doi ten

--- app/src/main/java/com/example/mymusicapp/queue/QueueActivity.kt
+++ app/src/main/java/com/example/mymusicapp/queue/QueueActivity.kt
@@ -9,28 +9,28 @@ import com.example.mymusicapp.R
 
 class QueueActivity : ComponentActivity() {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var songAdapter: SongAdapter
+    private lateinit var queueSongAdapter: QueueSongAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_queue)
 
         val songs = listOf(
-            Song("Song 1", "Artist 1"),
-            Song("Song 2", "Artist 2"),
-            Song("Song 3", "Artist 3"),
+            QueueSong("Song 1", "Artist 1"),
+            QueueSong("Song 2", "Artist 2"),
+            QueueSong("Song 3", "Artist 3"),
             // Add more songs here
         )
 
         recyclerView = findViewById(R.id.recyclerViewSongsQueue)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        songAdapter = SongAdapter(songs)
-        recyclerView.adapter = songAdapter
+        queueSongAdapter = QueueSongAdapter(songs)
+        recyclerView.adapter = queueSongAdapter
 
 
         recyclerView = findViewById(R.id.recyclerViewSongsPlaylist)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        songAdapter = SongAdapter(songs)
-        recyclerView.adapter = songAdapter
+        queueSongAdapter = QueueSongAdapter(songs)
+        recyclerView.adapter = queueSongAdapter
     }
 }
